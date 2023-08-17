abf2cc2808c03e821767d4fd683f3992fe99dc0f
Bootbastick <boikoff.nikita@gmail.com>
2023-08-17 14:24:02 +0200

    add debug output

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ app = Flask(__name__)
 #     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 # )
 
+print("Starting main app on 8080...")
 
 @app.route("/")
 def get_the_website():
     return render_template("index.html")
 
+
+print("Before start")
 
 if __name__ == "__main__":
     app.run(threaded=True, port=8080)
