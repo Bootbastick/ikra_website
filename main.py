07c27ebbf23b3ec59d524c430187ba5e6377f97b
Bootbastick <boikoff.nikita@gmail.com>
2023-08-17 14:16:06 +0200

    comment out werkzeug

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
-from werkzeug.middleware.proxy_fix import ProxyFix
+# from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
+# app.wsgi_app = ProxyFix(
+#     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+# )
 
 
 @app.route("/")
